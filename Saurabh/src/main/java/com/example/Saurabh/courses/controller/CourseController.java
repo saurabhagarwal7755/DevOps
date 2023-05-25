6658294d29e93543e9df068670ca3e0dfbddb298
Saurabh Kumar <52731148+saurabhagarwal7755@users.noreply.github.com>
2023-05-25 23:45:55 +0530

    Add files via upload

---
+++ Saurabh/src/main/java/com/example/Saurabh/courses/controller/CourseController.java
@@ -0,0 +1,18 @@
+package com.example.Saurabh.courses.controller;
+
+import com.example.Saurabh.courses.bean.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(new Course(1,"Learn Microservices","in28minutes"));
+    }
+
+}
